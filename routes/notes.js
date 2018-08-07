72143c028459f047c729d5b6c0286a78b413b15b
Tarik Desire <tprdesire@gmail.com>
2018-08-07 18:00:49 -0400

    Linked noteful-app db and refactored route handlers to use knex queries

--- routes/notes.js
+++ routes/notes.js
@@ -7,30 +7,44 @@ const router = express.Router();
 
 // TEMP: Simple In-Memory Database
 const data = require('../db/notes');
-const simDB = require('../db/simDB');
-const notes = simDB.initialize(data);
+//const simDB = require('../db/simDB');
+const knex = require('../knex');
+
+//const notes = simDB.initialize(data);
 
 // Get All (and search by query)
 router.get('/', (req, res, next) => {
   const { searchTerm } = req.query;
 
-  notes.filter(searchTerm)
-    .then(list => {
-      res.json(list);
+  knex
+    .select('notes.id', 'title', 'content')
+    .from('notes')
+    .modify(queryBuilder => {
+      if (searchTerm) {
+        queryBuilder.where('title', 'like', `%${searchTerm}%`);
+      }
+    })
+    .orderBy('notes.id')
+    .then(results => {
+      res.json(results);
     })
     .catch(err => {
       next(err);
     });
+
 });
 
 // Get a single item
 router.get('/:id', (req, res, next) => {
   const id = req.params.id;
 
-  notes.find(id)
-    .then(item => {
-      if (item) {
-        res.json(item);
+  knex
+    .select('notes.id', 'title', 'content')
+    .from('notes')
+    .where('notes.id', id)
+    .then(results => {
+      if (results.length) {
+        res.json(results[0]);
       } else {
         next();
       }
@@ -61,13 +75,12 @@ router.put('/:id', (req, res, next) => {
     return next(err);
   }
 
-  notes.update(id, updateObj)
-    .then(item => {
-      if (item) {
-        res.json(item);
-      } else {
-        next();
-      }
+  knex('notes')
+    .where('notes.id', id)
+    .update(updateObj)
+    .returning(['id', 'title', 'content'])
+    .then(result => {
+      res.json(result[0]);
     })
     .catch(err => {
       next(err);
@@ -85,23 +98,28 @@ router.post('/', (req, res, next) => {
     err.status = 400;
     return next(err);
   }
-
-  notes.create(newItem)
-    .then(item => {
-      if (item) {
-        res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+  
+  knex('notes')
+    .insert(newItem)
+    .returning(['id', 'title', 'content'])
+    .then(result => {
+      if (result) {
+        res.location(`http://${req.headers.host}/notes/${result[0].id}`).status(201).json(result[0]);
       }
     })
     .catch(err => {
       next(err);
     });
+
 });
 
 // Delete an item
 router.delete('/:id', (req, res, next) => {
   const id = req.params.id;
-
-  notes.delete(id)
+  
+  knex('notes')
+    .where('id', id)
+    .del()
     .then(() => {
       res.sendStatus(204);
     })
--- scratch/queries.js
+++ scratch/queries.js
@@ -18,3 +18,59 @@ knex
   .catch(err => {
     console.error(err);
   });
+
+let searchId = 1001;
+
+knex
+  .select('notes.id', 'title', 'content')
+  .from('notes')
+  .where('notes.id', searchId)
+  .then(results => {
+    console.log(JSON.stringify(results[0]));
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+let updateTitle = 'UpdateTitle';
+let updateContent = 'Update content';
+
+knex('notes')
+  .where('notes.id', searchId)
+  .update({
+    title: updateTitle,
+    content: updateContent
+  })
+  .returning(['id', 'title', 'content'])
+  .then(result => {
+    console.log(JSON.stringify(result[0]));
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+
+let createdTitle = 'New Title';
+let createdContent = 'New Content';
+
+knex('notes')
+  .insert({title: createdTitle, content: createdContent})
+  .returning(['id', 'title', 'content'])
+  .then(result => {
+    console.log(JSON.stringify(result[0]));
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+let deleteId = 1010;
+
+knex('notes')
+  .where('id', deleteId)
+  .del()
+  .then(() => {
+    console.log(`Note with Id: ${deleteId} was deleted`);
+  })
+  .catch(err => {
+    console.error(err);
+  });
